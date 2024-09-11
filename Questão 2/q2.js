5e2aadec6f974eabe9d32ec2c331e738c5c991b6
R1luc <137064816+R1luc@users.noreply.github.com>
2024-09-11 15:00:30 -0300

    Update q2.js

--- Questão 2/q2.js
+++ Questão 2/q2.js
@@ -1,5 +1,4 @@
 // QUESTÃO: Fazer uma requisição para a API e exibir os detalhes de um jogo específico.
-
 const matches = 'https://worldcupjson.net/matches';
 fetch(matches)
   .then(response => response.json())
@@ -27,7 +26,7 @@ function displayMatches(matches){
     
         const matchElement = document.createElement('div');
         matchElement.innerHTML = `
-        <h2>Detalhes do Jogo:</h2>
+        <h2><b>Detalhes Gerais:</b></h2>
         <p><b>ID:</b> ${matches.id}</p>
         <p><b>Estádio:</b> ${matches.venue}, ${matches.location}</p>
         <p><b>Status:</b> ${matches.status}</p>
@@ -39,26 +38,26 @@ function displayMatches(matches){
         <p><b>Vencedor:</b> ${matches.winner}</p>
         <p><b>Sigla do Vencedor:</b> ${matches.winner_code}</p>
         
-        <h2>Equipe da Casa</h2>
+        <h2>Equipe da Casa:</h2>
         <p><b>País:</b> ${matches.home_team.country}</p>
         <p><b>Nome:</b> ${matches.home_team.name}</p>
         <p><b>Gols:</b> ${matches.home_team.goals}</p>
         <p><b>Pênaltis:</b> ${matches.home_team.penalties}</p>
         
-        <h2>Equipe Visitante</h2>
+        <h2>Equipe Visitante:</h2>
         <p><b>País:</b> ${matches.away_team.country}</p>
         <p><b>Nome:</b> ${matches.away_team.name}</p>
         <p><b>Gols:</b> ${matches.away_team.goals}</p>
         <p><b>Pênaltis:</b> ${matches.away_team.penalties}</p>
         
-        <h2>Clima</h2>
+        <h2>Clima:</h2>
         <p><b>Umidade:</b> ${matches.weather.humidity}%</p>
         <p><b>Temperatura (Celsius):</b> ${matches.weather.temp_celsius}°C</p>
         <p><b>Temperatura (Fahrenheit):</b> ${matches.weather.temp_farenheit}°F</p>
         <p><b>Velocidade do Vento:</b> ${matches.weather.wind_speed} km/h</p>
         <p><b>Descrição:</b> ${matches.weather.description}</p>
         
-        <h2>Tempo</h2>
+        <h2>Tempo:</h2>
         <p><b>Tempo:</b> ${matches.time}</p>
         <p><b>Tempo Atual:</b> ${matches.detailed_time.current_time}</p>
         <p><b>Primeiro Tempo:</b> ${matches.detailed_time.first_half_time}</p>
@@ -66,7 +65,7 @@ function displayMatches(matches){
         <p><b>Segundo Tempo:</b> ${matches.detailed_time.second_half_time}</p>
         <p><b>Extra Tempo do Segundo Tempo:</b> ${matches.detailed_time.second_half_extra_time}</p>
         
-        <h2>Árbitros</h2>
+        <h2>Árbitros:</h2>
         <ul>
           <li><b>Nome:</b> ${matches.officials[0].name} - <b>Função:</b> ${matches.officials[0].role} - <b>País:</b> ${matches.officials[0].country}</li>
           <li><b>Nome:</b> ${matches.officials[1].name} - <b>Função:</b> ${matches.officials[1].role} - <b>País:</b> ${matches.officials[1].country}</li>
@@ -79,17 +78,17 @@ function displayMatches(matches){
           <li><b>Nome:</b> ${matches.officials[8].name} - <b>Função:</b> ${matches.officials[8].role} - <b>País:</b> ${matches.officials[8].country}</li>
         </ul>
         
-        <h2>Eventos da Equipe da Casa</h2>
+        <h2>Eventos da Equipe da Casa:</h2>
         <ul>
           ${matches.home_team_events.map(event => `<li><b>Tipo:</b> ${event.type_of_event} - <b>Jogador:</b> ${event.player} - <b>Tempo:</b> ${event.time}</li>`).join('')}
         </ul>
         
-        <h2>Eventos da Equipe Visitante</h2>
+        <h2>Eventos da Equipe Visitante:</h2>
         <ul>
           ${matches.away_team_events.map(event => `<li><b>Tipo:</b> ${event.type_of_event} - <b>Jogador:</b> ${event.player} - <b>Tempo:</b> ${event.time}</li>`).join('')}
         </ul>
         
-        <h2>Escalação da Equipe da Casa</h2>
+        <h2>Escalação da Equipe da Casa:</h2>
         <p><b>País:</b> ${matches.home_team_lineup.country}</p>
         <p><b>Tática:</b> ${matches.home_team_lineup.tactics}</p>
         <h3>Títulares</h3>
@@ -101,7 +100,7 @@ function displayMatches(matches){
           ${matches.home_team_lineup.substitutes.map(player => `<li><b>Nome:</b> ${player.name} - <b>Número:</b> ${player.shirt_number} - <b>Posição:</b> ${player.position}</li>`).join('')}
         </ul>
         
-        <h2>Escalação da Equipe Visitante</h2>
+        <h2>Escalação da Equipe Visitante:</h2>
         <p><b>País:</b> ${matches.away_team_lineup.country}</p>
         <p><b>Tática:</b> ${matches.away_team_lineup.tactics}</p>
         <h3>Títulares</h3>
@@ -113,7 +112,7 @@ function displayMatches(matches){
           ${matches.away_team_lineup.substitutes.map(player => `<li><b>Nome:</b> ${player.name} - <b>Número:</b> ${player.shirt_number} - <b>Posição:</b> ${player.position}</li>`).join('')}
         </ul>
         
-        <h2>Estatísticas da Equipe da Casa</h2>
+        <h2>Estatísticas da Equipe da Casa:</h2>
         <p><b>País:</b> ${matches.home_team_statistics.country}</p>
         <p><b>Tentativas de Gol:</b> ${matches.home_team_statistics.attempts_on_goal}</p>
         <p><b>Chutes ao Gol Contra:</b> ${matches.home_team_statistics.attempts_on_goal_against}</p>
@@ -134,7 +133,7 @@ function displayMatches(matches){
         <p><b>Cartões Vermelhos:</b> ${matches.home_team_statistics.red_cards}</p>
         <p><b>Faltas Cometidas:</b> ${matches.home_team_statistics.fouls_committed}</p>
         
-        <h2>Estatísticas da Equipe Visitante</h2>
+        <h2>Estatísticas da Equipe Visitante:</h2>
         <p><b>País:</b> ${matches.away_team_statistics.country}</p>
         <p><b>Tentativas ao Gol:</b> ${matches.away_team_statistics.attempts_on_goal}</p>
         <p><b>Chutes ao Gol Contra:</b> ${matches.away_team_statistics.attempts_on_goal_against}</p>
