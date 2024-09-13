7952fd45ce5e77ba0110a3c8ee84d10fe7930401
LucasPLimal <pereira.lima1@escolar.ifrn.eu.br>
2024-09-13 08:01:09 -0300

    q4 comentada

--- Questão 4/q4.js
+++ Questão 4/q4.js
@@ -1,19 +1,19 @@
 const url = 'https://worldcupjson.net/teams';
 
 fetch(url)
-    .then(response => response.json())
-    .then(classificacao => lista(classificacao));
+    .then(response => response.json()) //Puxar o JSON da API
+    .then(classificacao => lista(classificacao)); 
 
 function lista(classificacao) {
-    const tabelas = document.getElementById("tabela");
+    const tabelas = document.getElementById("tabela"); //Cria uma constante do elemento HTML "tabela"
     
-    classificacao.groups.forEach(group => {
+    classificacao.groups.forEach(group => { //Cria elementos da tabela para cada grupo presente em grupos
         let tabela = document.createElement("table");
         let head = document.createElement("thead");
         let title = document.createElement("h3");
 
-        tabelas.appendChild(title);
-        title.innerHTML = `<h3>Grupo ${group.letter}</h3>`;
+        tabelas.appendChild(title); //Torna a variavel "title" como um elemento de "tabelas"
+        title.innerHTML = `<h3>Grupo ${group.letter}</h3>`; //Cria o título de cada tabela
         
         head.innerHTML = `
             <tr>
@@ -27,19 +27,15 @@ function lista(classificacao) {
                 <th>Gols contra</th>
                 <th>Diferença</th>
             </tr>
-        `;
-        tabela.appendChild(head);
+        `; //Estrutura do cabeçalho tabela
+        tabela.appendChild(head); //Torna a variavel "head" como um elemento de "tabela"
 
-        // Ordena os times por pontos em ordem decrescente
-        group.teams.sort((a, b) => b.group_points - a.group_points);
+        group.teams.sort((a, b) => b.group_points - a.group_points); // Ordena os times por pontos em ordem decrescente
 
         group.teams.forEach(team => {
-            let line = tabela.insertRow();
+            let line = tabela.insertRow(); //Adiciona uma celula(coluna)
 
-            let firstcell = line.insertCell(0);
-            firstcell.style.width = "100px";
-            firstcell.textContent = team.name;
-
+            line.insertCell(0).textContent = team.name;
             line.insertCell(1).textContent = team.group_points;
             line.insertCell(2).textContent = team.wins;
             line.insertCell(3).textContent = team.draws;
@@ -47,16 +43,16 @@ function lista(classificacao) {
             line.insertCell(5).textContent = team.games_played;
             line.insertCell(6).textContent = team.goals_for;
             line.insertCell(7).textContent = team.goals_against;
-            line.insertCell(8).textContent = team.goal_differential;
+            line.insertCell(8).textContent = team.goal_differential; //Adiciona informações da API de acordo com cada coluna do Cabeçalho
         });
 
-        tabela.style.borderWidth = "1px";
-        tabela.style.borderColor = "#555";
-        tabela.style.borderStyle = "solid";
-        tabela.style.background = "#ffefef";
-        tabela.style.textAlign = "center";
-        tabela.style.width = "100%";
+        tabela.style.borderWidth = "1px"; //Pixels da borda
+        tabela.style.borderColor = "#555"; //Cor da borda
+        tabela.style.borderStyle = "solid"; //Tipo da borda
+        tabela.style.background = "#ffefef"; //Background
+        tabela.style.textAlign = "center"; // Alinhamento do texto
+        tabela.style.width = "100%"; //Altera o preenchimento 
 
-        tabelas.appendChild(tabela);
+        tabelas.appendChild(tabela); //Torna a variavel "tabela" como um elemento de "tabelas"
     });
 }
